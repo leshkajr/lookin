67e31ee7399d370209b5ebd4d677d173c2c91114
Алексей Игнатенко <ignatenkolesha123@gmail.com>
2023-08-08 02:35:58 +0200

    breeze install

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\App;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    function welcome() {
+        $locale = App::currentLocale();
+        return view('welcome')->with("locale",$locale);
+    }
 }
