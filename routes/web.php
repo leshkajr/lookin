7293122e65cfb5fdaa18aaf0479a4f25df23fa8c
Алексей Игнатенко <ignatenkolesha123@gmail.com>
2023-08-15 16:46:23 +0200

    Add route search with MainController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use \App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/search', [MainController::class, 'index'])->name('main.search');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
