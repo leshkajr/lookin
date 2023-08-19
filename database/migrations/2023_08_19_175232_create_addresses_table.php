e0d80d9dc9a8a2bfae4422e960d9a1388ad6ed67
Алексей Игнатенко <ignatenkolesha123@gmail.com>
2023-08-19 20:01:58 +0200

    add guarded

---
+++ database/migrations/2023_08_19_175232_create_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstLine'); // Ukrainska 2
+            $table->string('secondLine'); // 89
+            $table->string('index'); // 00-350
+            $table->string('city'); // Kryvyi Rih
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
