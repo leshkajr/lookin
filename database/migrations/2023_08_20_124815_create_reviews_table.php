31785ed1ab9288eb523636ca901f576c89f6addb
Алексей Игнатенко <ignatenkolesha123@gmail.com>
2023-08-20 15:20:00 +0200

    Create and fill migration Review

---
+++ database/migrations/2023_08_20_124815_create_reviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger("userId");
+            $table->string("reviewText",256);
+            $table->unsignedFloat("overallRating");
+            $table->unsignedFloat("cleanlinessRating");
+            $table->unsignedFloat("accuracyRating");
+            $table->unsignedFloat("communicationRating");
+            $table->unsignedFloat("locationRating");
+            $table->unsignedFloat("checkInRating");
+            $table->unsignedFloat("priceQualityRatioRating");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
