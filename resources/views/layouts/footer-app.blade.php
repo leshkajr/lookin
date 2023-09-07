b3707e04ead7cdc6327d3e2a723616329f026ffb
Ivan Pashko <ivan.pashko1996@gmail.com>
2023-09-07 20:14:31 +0300

    добавил footer-app в папку layout

---
+++ resources/views/layouts/footer-app.blade.php
@@ -0,0 +1,41 @@
+<footer>
+    <div class="footer-container">
+        <div class="row-likes">
+            <div class="footer-likes">
+                <h5>Поддержка</h5>
+                <ul>
+                    <li><a href="#">Центр помощи</a></li>
+                    <li><a href="#">AirCover</a></li>
+                    <li><a href="#">Борьба с дискриминацией</a></li>
+                    <li><a href="#">Помощь людям с инвалидностью</a></li>
+                    <li><a href="#">Сообщить о проблеме в районе</a></li>
+                </ul>
+            </div>
+            <div class="footer-likes">
+                <h5>Прием гостей</h5>
+                <ul>
+                    <li><a href="#">Сдайте жилье на Airbnb</a></li>
+                    <li><a href="#">AirCover для хозяев</a></li>
+                    <li><a href="#">Материалы для хозяев</a></li>
+                    <li><a href="#">Форум сообщества</a></li>
+                    <li><a href="#">Ответственный прием гостей</a></li>
+                </ul>
+            </div>
+            <div class="footer-likes">
+                <h5>lookin</h5>
+                <ul>
+                    <li><a href="#">Пресс-центр</a></li>
+                    <li><a href="#">Новые функции</a></li>
+                    <li><a href="#">Карьера в Airbnb</a></li>
+                    <li><a href="#">Для инвесторов</a></li>
+                    <li><a href="#">Прием гостей на Airbnb.org</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    {{--        <hr style="color: #1a1d20">--}}
+    {{--        <div style="text-align: center; marg">--}}
+    {{--            <p class="p-likes">&copy; 2023 lookin, Inc.</p>--}}
+    {{--            <p class="p-likes">Конфиденциальность | Условия | Карта сайта | Реквизиты компании</p>--}}
+    {{--        </div>--}}
+</footer>
