4aebf2e890462d941dd608772c043a2dc269fcd8
Алексей Игнатенко <ignatenkolesha123@gmail.com>
2023-10-01 16:09:47 +0200

    countries cities add do database ура получилось

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -16,28 +16,36 @@ class Country extends Model
             $countries = json_decode(file_get_contents("https://countriesnow.space/api/v0.1/countries"));
 
             foreach ($countries->data as $country) {
-                if (!self::isExist(self::All(), $country->country)) {
                     $create_country = array(
                         'name' => $country->country,
                         'iso2' => strtolower($country->iso2),
                     );
                     $id = self::create($create_country)->id;
-                    $cities = json_decode(file_get_contents("http://api.geonames.org/searchJSON?country=".$country->iso2."&featureClass=P&username=oleksii.ihnatenko"))->geonames;
-
-                    usort($cities, function($a, $b) { return -1 * strcmp($a->population, $b->population); });
-                    if(count($cities) < 20) {
+                    $cities = json_decode(file_get_contents("http://api.geonames.org/searchJSON?country=".$country->iso2."&featureClass=P&username=oleksii.ihnatenko"));
+                    if(isset($cities->geonames)){
+                        $cities = $cities->geonames;
+//                    usort($cities, function($a, $b) {
+//                        if($a->population > $b->population) return 1;
+//                        else return 0;
+//                    });
+//                    if($country->country === 'Ukraine'){
+//                        dd($cities);
+//                    }
+                    if(count($cities) < 50) {
                         $countCities = count($cities);
                     }
                     else {
-                        $countCities = 20;
+                        $countCities = 50;
                     }
 
                     for ($i = 0; $i < $countCities; $i++) {
-                        $create_city = array(
-                            'name' => $cities[$i]->name,
-                            'countryId' => $id,
-                        );
-                        City::create($create_city);
+                        if($cities[$i]->population > 150000){
+                            $create_city = array(
+                                'name' => $cities[$i]->name,
+                                'countryId' => $id,
+                            );
+                            City::create($create_city);
+                        }
                     }
                 }
             }
